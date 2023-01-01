44af0962e45e7614415cde51fbde038fb6e8ace7
yelkarasidogancan <yelkarasidogancan@gmail.com>
2023-01-01 23:15:09 +0300

    country

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from "react";
+
+import { RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
+
+import { Menu } from "@headlessui/react";
+
+import { HouseContext } from "./HouseContext";
 
 const CountryDropdown = () => {
-  return <div>CountryDropdown</div>;
+  const { country, setCountry, countries } = useContext(HouseContext);
+
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Menu as="div" className="dropdown relative">
+      <Menu.Button className="dropdown-btn w-full text-left">
+        <RiMapPinLine className="dropdown-icon-primary" />
+        <div>
+          <div>{country}</div>
+          <div>Select your place</div>
+          {isOpen ? (
+            <RiArrowUpSLine className="dropdown-icon-primary" />
+          ) : (
+            <RiArrowDownSLine className="dropdown-icon-primary" />
+          )}
+        </div>
+      </Menu.Button>
+    </Menu>
+  );
 };
 
 export default CountryDropdown;
--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from "react";
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+import { housesData } from "../data";
+
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState("Location (any)");
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState("Property type (any)");
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState("Price range (any)");
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <HouseContext.Provider
+      value={{
+        country,
+        setCountry,
+        countries,
+        property,
+        setProperty,
+        properties,
+        price,
+        setPrice,
+        houses,
+        loading,
+      }}
+    >
+      {children}
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
