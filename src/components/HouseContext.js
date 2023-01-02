0707433fcf0cd3ec69d3b088d9b39a8f73d8926c
yelkarasidogancan <yelkarasidogancan@gmail.com>
2023-01-02 16:26:57 +0300

    house_filter

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -32,7 +32,59 @@ const HouseContextProvider = ({ children }) => {
   }, []);
 
   const handleClick = () => {
-    console.log("clicked");
+    setLoading(true);
+    // create a function
+    const isDefault = (str) => {
+      return str.split(" ").includes("(any)");
+    };
+
+    const minPrice = parseInt(price.split(" ")[0]);
+    const maxPrice = parseInt(price.split(" ")[2]);
+
+    const newHouses = housesData.filter((house) => {
+      const housePrice = parseInt(house.price);
+      if (
+        house.country === country &&
+        house.type === property &&
+        housePrice >= minPrice &&
+        housePrice <= maxPrice
+      ) {
+        return house;
+      }
+      if (isDefault(country) && isDefault(property) && isDefault(price)) {
+        return house;
+      }
+      if (!isDefault(country) && isDefault(property) && isDefault(price)) {
+        return house.country === country;
+      }
+      if (!isDefault(property) && isDefault(country) && isDefault(price)) {
+        return house.type === property;
+      }
+      if (!isDefault(price) && isDefault(country) && isDefault(property)) {
+        if (housePrice >= minPrice && housePrice <= maxPrice) {
+          return house;
+        }
+      }
+      if (!isDefault(country) && !isDefault(property) && isDefault(price)) {
+        return house.country === country && house.type === property;
+      }
+      if (!isDefault(country) && isDefault(property) && !isDefault(price)) {
+        if (housePrice >= minPrice && housePrice <= maxPrice) {
+          return house.country === country;
+        }
+      }
+      if (isDefault(country) && !isDefault(property) && !isDefault(price)) {
+        if (housePrice >= minPrice && housePrice <= maxPrice) {
+          return house.type === property;
+        }
+      }
+    });
+    setTimeout(() => {
+      return (
+        newHouses.length < 1 ? setHouses([]) : setHouses(newHouses),
+        setLoading(false)
+      );
+    }, 1000);
   };
   return (
     <HouseContext.Provider
@@ -48,6 +100,7 @@ const HouseContextProvider = ({ children }) => {
         houses,
         loading,
         handleClick,
+        loading,
       }}
     >
       {children}
--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -11,10 +11,21 @@ import { ImSpinner2 } from "react-icons/im";
 const HouseList = () => {
   const { houses, loading } = useContext(HouseContext);
 
-  console.log(houses);
+  if (loading) {
+    return (
+      <ImSpinner2 className="mx-auto animate-spin text-violet-700 text-4xl mt-[200px]"></ImSpinner2>
+    );
+  }
+  if (houses.length < 1) {
+    return (
+      <div className="text-center text-3xl text-gray-400 mt-48">
+        Sorry, nothing found
+      </div>
+    );
+  }
 
   return (
-    <section className="mb-20">
+    <section className="mb-20 py-10">
       <div className="container mx-auto">
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-14">
           {houses.map((house, index) => {
